b3fa558a69480a1f5bc0284cb9bcb2ad383356d4
anjali29singh <anjalisingh930260@gmail.com>
2023-07-23 13:04:28 +0530

    .

--- cses/graph/Flight_Discount.cpp
+++ cses/graph/Flight_Discount.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int N, M;
 #define ll long long
-ll INTMAX = 1e20 + 1;
+ll INTMAX = 1e15;
 typedef pair<ll, int> ipair;
 
 // meet in the middle problem .
 //  start dijikstra's from 1 and store result in vector<ll> front;
 // start dijikstra's from n and store result in vecotr<ll> back;
 // for each edge u->v take minimum front[u]+ wt/2+back[v];
-void dijikstra(int src, vector<vector<pair<int, int>>> adj, vector<ll> &dist)
+void djkf(int src, vector<vector<pair<int, int>>> adj, vector<ll> &dist)
 {
 
     priority_queue<ipair, vector<ipair>, greater<ipair>> pq;
@@ -22,9 +22,37 @@ void dijikstra(int src, vector<vector<pair<int, int>>> adj, vector<ll> &dist)
     {
         int u = pq.top().second;
         pq.pop();
+
         for (auto i : adj[u])
         {
             int v = i.first, wt = i.second;
+
+            if (dist[v] > dist[u] + (ll)wt)
+            {
+                dist[v] = dist[u] + (ll)wt;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+}
+void djkn(int src, vector<vector<pair<int, int>>> adj, vector<ll> &dist)
+{
+
+    priority_queue<ipair, vector<ipair>, greater<ipair>> pq;
+
+    pq.push(make_pair(0, src));
+
+    dist[src] = 0;
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        for (auto i : adj[u])
+        {
+            int v = i.first, wt = i.second;
+
             if (dist[v] > dist[u] + (ll)wt)
             {
                 dist[v] = dist[u] + (ll)wt;
@@ -53,8 +81,8 @@ int main()
 
     vector<ll> front(N + 1, INTMAX);
     vector<ll> back(N + 1, INTMAX);
-    dijikstra(1, adj1, front);
-    dijikstra(N, adj2, back);
+    djkf(1, adj1, front);
+    djkn(N, adj2, back);
 
     ll ans = INTMAX;
     for (auto i : edges)
